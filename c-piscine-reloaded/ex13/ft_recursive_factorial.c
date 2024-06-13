d27f1f410f3318a1d4d51691cb0917b42f8c6858
Daniel Samuel <dansam5000@gmail.com>
2024-06-14 00:28:43 +0200

    ex12 recursive fact reloaded

---
+++ c-piscine-reloaded/ex13/ft_recursive_factorial.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dsamuel <dsamuel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/05 10:30:09 by dsamuel           #+#    #+#             */
+/*   Updated: 2024/06/05 10:32:24 by dsamuel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_recursive_factorial(int nb)
+{
+	if (nb < 0)
+		return (0);
+	else if (nb <= 1)
+		return (1);
+	else
+		return (nb * ft_recursive_factorial(nb - 1));
+}
