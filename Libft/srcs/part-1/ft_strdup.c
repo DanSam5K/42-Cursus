01f796ad4a0927caf86fa4406a3ccaa8075713e2
Daniel Samuel <dansam5000@gmail.com>
2024-07-01 23:57:47 +0200

    ft_strdup

---
+++ Libft/srcs/part-1/ft_strdup.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dansam <dansam@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/10 21:11:01 by dansam            #+#    #+#             */
+/*   Updated: 2024/06/11 20:09:25 by dansam           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_strdup(const char *s1)
+{
+	char *s2;
+	size_t len;
+
+	len = ft_strlen(s1) + 1;
+	s2 = (char *)malloc(len);
+	if (!s2)
+		return (NULL);
+	return ((char *)ft_memcpy(s2, s1, len));
+}
