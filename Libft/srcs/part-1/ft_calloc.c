60d6765e8dfb67e339d3970572170c9011c3415c
Daniel Samuel <dansam5000@gmail.com>
2024-07-05 22:01:18 +0200

    str cat

---
+++ Libft/srcs/part-1/ft_calloc.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dansam <dansam@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/02 15:25:43 by dansam            #+#    #+#             */
+/*   Updated: 2024/07/02 17:31:16 by dansam           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void *ft_calloc(size_t count, size_t size)
+{
+    size_t total_size;
+    void *dst;
+    unsigned char *ptr;
+    size_t i;
+
+    if (count == 0 || size == 0)
+        return (malloc(0));
+    if (count > SIZE_MAX / size)
+        return (NULL);
+    total_size = count * size;
+    ptr = malloc(total_size);
+    if (dst == NULL)
+        return (NULL);
+    ptr = (unsigned char *)dst;
+    i = 0;
+    while (i < total_size)
+    {
+        ptr[i] = 0;
+        i++;
+    }
+    return (dst);
+}
+
+int main(void)
+{
+    size_t count = 5;
+    size_t size = 4;
+    size_t total_size = count * size;
+    void *ptr = ft_calloc(count, size);
+    unsigned char *ptr_c = (unsigned char *)ptr;
+    size_t i = 0;
+
+    printf("calloc: ");
+    while (i < total_size)
+    {
+        printf("%d ", ptr_c[i]);
+        i++;
+    }
+    printf("\n");
+    free(ptr);
+    return (0);
+}
---
+++ Libft/srcs/part-1/ft_strlcat.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dansam <dansam@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/12 21:40:25 by dansam            #+#    #+#             */
+/*   Updated: 2024/06/12 21:40:42 by dansam           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t i;
+	size_t j;
+	size_t len;
+
+	i = 0;
+	j = 0;
+	len = ft_strlen(dst) + ft_strlen(src);
+	if (dstsize <= ft_strlen(dst))
+		return (ft_strlen(src) + dstsize);
+	while (dst[i])
+		i++;
+	while (src[j] && i + j < dstsize - 1)
+	{
+		dst[i + j] = src[j];
+		j++;
+	}
+	dst[i + j] = '\0';
+	return (len);
+}
