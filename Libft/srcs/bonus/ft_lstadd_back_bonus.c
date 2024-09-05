75858b89c9cd0cfdb60131a2fed5b1eab76f0464
Daniel Samuel <dansam5000@gmail.com>
2024-09-05 23:17:55 +0200

    test list add back

--- Libft/srcs/bonus/ft_lstadd_back_bonus.c
+++ Libft/srcs/bonus/ft_lstadd_back_bonus.c
@@ -3,14 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dsamuel <dsamuel@student.42.fr>            +#+  +:+       +#+        */
+/*   By: dansam <dansam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 19:34:44 by dsamuel           #+#    #+#             */
-/*   Updated: 2024/06/18 19:49:23 by dsamuel          ###   ########.fr       */
+/*   Updated: 2024/09/05 22:53:16 by dansam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "../../includes/libft.h"
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
@@ -27,4 +27,19 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 	while (temp->next)
 		temp = temp->next;
 	temp->next = new;
+}
+
+int main(void)
+{
+	t_list *lst = ft_lstnew(ft_strdup("first"));
+	ft_lstadd_back(&lst, ft_lstnew(ft_strdup("second")));
+	ft_lstadd_back(&lst, ft_lstnew(ft_strdup("third")));
+	ft_lstadd_back(&lst, ft_lstnew(ft_strdup("fourth")));
+	ft_lstadd_back(&lst, ft_lstnew(ft_strdup("fifth")));
+	while (lst)
+	{
+		printf("%s\n", (char *)lst->content);
+		lst = lst->next;
+	}
+	return (0);
 }
