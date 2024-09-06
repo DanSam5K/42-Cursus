ea89d7c7193d89d03ebc22c2c7cbd17f7fb8af7e
Daniel Samuel <dansam5000@gmail.com>
2024-09-06 22:06:46 +0200

    update solution

--- Libft/srcs/part-1/ft_memchr.c
+++ Libft/srcs/part-1/ft_memchr.c
@@ -3,14 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dsamuel <dsamuel@student.42.fr>            +#+  +:+       +#+        */
+/*   By: dansam <dansam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 21:02:30 by dsamuel           #+#    #+#             */
-/*   Updated: 2024/06/13 21:55:08 by dsamuel          ###   ########.fr       */
+/*   Updated: 2024/09/06 22:06:13 by dansam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "../../includes/libft.h"
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
@@ -25,3 +25,17 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	}
 	return (0);
 }
+
+// #include <stdio.h>
+// int	main(void)
+// {
+// 	char	str[10] = "Hello";
+// 	char	*ptr;
+
+// 	ptr = ft_memchr(str, 'a', 5);
+// 	if (ptr)
+// 		printf("Character found: %c\n", *ptr);
+// 	else
+// 		printf("Character not found\n");
+// 	return (0);
+// }
