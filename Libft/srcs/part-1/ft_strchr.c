e224aabb77d2b923e9a94b8bc4605044006eedcd
Daniel Samuel <dansam5000@gmail.com>
2024-09-08 14:26:46 +0200

    ft_memset and strchr

--- Libft/srcs/part-1/ft_strchr.c
+++ Libft/srcs/part-1/ft_strchr.c
@@ -3,12 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dsamuel <dsamuel@student.42.fr>            +#+  +:+       +#+        */
+/*   By: dansam <dansam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 10:50:22 by dsamuel           #+#    #+#             */
-/*   Updated: 2024/06/19 18:53:55 by dsamuel          ###   ########.fr       */
+/*   Updated: 2024/09/08 13:37:41 by dansam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "../../includes/libft.h"
 
 char	*ft_strchr(const char *s, int c)
 {
@@ -23,5 +25,19 @@ char	*ft_strchr(const char *s, int c)
 	}
 	if (uc == '\0')
 		return ((char *)s);
+	return (0);
+}
+
+#include <stdio.h>
+int	main(void)
+{
+	char	str[10] = "Hello";
+	char	*ptr;
+
+	ptr = ft_strchr(str, 'a');
+	if (ptr)
+		printf("Character found: %c\n", *ptr);
+	else
+		printf("Character not found\n");
 	return (0);
 }
