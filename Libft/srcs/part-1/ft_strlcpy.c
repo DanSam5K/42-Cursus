e326f371924f9939aac9ce4fc817b52de4b3a82b
Daniel Samuel <dansam5000@gmail.com>
2024-09-09 22:27:18 +0200

    strlcpy

--- Libft/srcs/part-1/ft_strlcpy.c
+++ Libft/srcs/part-1/ft_strlcpy.c
@@ -3,14 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dsamuel <dsamuel@student.42.fr>            +#+  +:+       +#+        */
+/*   By: dansam <dansam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 11:41:15 by dsamuel           #+#    #+#             */
-/*   Updated: 2024/06/12 21:10:33 by dsamuel          ###   ########.fr       */
+/*   Updated: 2024/09/09 22:26:16 by dansam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "../../includes/libft.h"
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t n)
 {
@@ -26,3 +26,15 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t n)
 	*dst = '\0';
 	return (i);
 }
+
+// #include <stdio.h>
+// int	main(void)
+// {
+// 	char	dst[50] = "Hello";
+// 	char	src[50] = "World";
+// 	size_t	n;
+
+// 	n = 10;
+// 	printf("%lu\n", ft_strlcpy(dst, src, n));
+// 	return (0);
+// }
