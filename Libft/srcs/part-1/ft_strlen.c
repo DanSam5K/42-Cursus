7579d0d0970f8a5bc0807e80f5033a8625679c04
Daniel Samuel <dansam5000@gmail.com>
2024-09-10 05:32:20 +0200

    ft strelen func

--- Libft/srcs/part-1/ft_strlen.c
+++ Libft/srcs/part-1/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: dansam <dansam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 12:12:20 by dsamuel           #+#    #+#             */
-/*   Updated: 2024/09/04 22:01:47 by dansam           ###   ########.fr       */
+/*   Updated: 2024/09/10 05:31:29 by dansam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,3 +21,12 @@ size_t	ft_strlen(const char *s)
 		i++;
 	return (i);
 }
+
+// int main(void)
+// {
+// 	char	*str;
+
+// 	str = "Hello";
+// 	printf("%lu\n", ft_strlen(str));
+// 	return (0);
+// }
