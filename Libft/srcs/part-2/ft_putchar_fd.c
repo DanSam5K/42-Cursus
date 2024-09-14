65a15ddf61b2a2d4d16e989e4adbde40c8b41280
Daniel Samuel <dansam5000@gmail.com>
2024-09-14 10:59:15 +0200

    puthcar fd

--- Libft/srcs/part-2/ft_putchar_fd.c
+++ Libft/srcs/part-2/ft_putchar_fd.c
@@ -3,16 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putchar_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dsamuel <dsamuel@student.42.fr>            +#+  +:+       +#+        */
+/*   By: dansam <dansam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 11:50:26 by dsamuel           #+#    #+#             */
-/*   Updated: 2024/06/17 11:51:48 by dsamuel          ###   ########.fr       */
+/*   Updated: 2024/09/14 10:58:48 by dansam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "../../includes/libft.h"
 
 void	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
 }
+
+// int main(void)
+// {
+// 	ft_putchar_fd('a', 1);
+// 	ft_putchar_fd('\n', 1);
+// 	return (0);
+// }
