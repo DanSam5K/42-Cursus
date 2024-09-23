fb4cde0f622fff5f1ad4fe1f14a268442e6a51fb
Daniel Samuel <dansam5000@gmail.com>
2024-09-23 22:38:39 +0200

    Strmapi

--- Libft/srcs/part-2/ft_strmapi.c
+++ Libft/srcs/part-2/ft_strmapi.c
@@ -6,21 +6,21 @@
 /*   By: dansam <dansam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/16 16:57:10 by dsamuel           #+#    #+#             */
-/*   Updated: 2024/09/23 20:12:23 by dansam           ###   ########.fr       */
+/*   Updated: 2024/09/23 22:38:18 by dansam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/libft.h"
 
-size_t	ft_strlen(const char *s)
-{
-	size_t	len;
+// size_t	ft_strlen(const char *s)
+// {
+// 	size_t	len;
 
-	len = 0;
-	while (s[len])
-		len++;
-	return (len);
-}
+// 	len = 0;
+// 	while (s[len])
+// 		len++;
+// 	return (len);
+// }
 
 static char	*ft_str_new(size_t n)
 {
@@ -51,21 +51,16 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	return (result);
 }
 
-// char	ft_toupper(unsigned int i, char c)
+// char	ft_print_strmapi(unsigned int index, char c)
 // {
-// 	(void)i;
-// 	if (c >= 'a' && c <= 'z')
-// 		return (c - 32);
+// 	printf("index: %d, char: %c\n", index, c);
 // 	return (c);
 // }
 
 // int main(void)
 // {
-// 	char	*str = "Hello, World!";
-// 	char	*result;
-
-// 	result = ft_strmapi(str, ft_toupper);
-// 	printf("%s\n", result);
-// 	free(result);
+// 	char *s = "Hello, World!";
+// 	char *new_s = ft_strmapi(s, ft_print_strmapi);
+// 	printf("%s\n", new_s);
 // 	return (0);
 // }
