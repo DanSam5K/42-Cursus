a610a36651c23eb8f56b1dc8f2c18d1484a7f4a6
Daniel Samuel <dansam5000@gmail.com>
2024-09-26 17:16:38 +0200

    update lst add back

--- Libft/srcs/bonus/ft_lstadd_front_bonus.c
+++ Libft/srcs/bonus/ft_lstadd_front_bonus.c
@@ -3,14 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstadd_front_bonus.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dsamuel <dsamuel@student.42.fr>            +#+  +:+       +#+        */
+/*   By: dansam <dansam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 19:13:37 by dsamuel           #+#    #+#             */
-/*   Updated: 2024/06/19 12:28:54 by dsamuel          ###   ########.fr       */
+/*   Updated: 2024/09/26 17:15:53 by dansam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "../../includes/libft.h"
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
@@ -19,3 +19,61 @@ void	ft_lstadd_front(t_list **lst, t_list *new)
 	new->next = *lst;
 	*lst = new;
 }
+
+// implementation of ft_strlen
+// size_t	ft_strlen(const char *s)
+// {
+// 	size_t	len;
+
+// 	len = 0;
+// 	while (s[len])
+// 		len++;
+// 	return (len);
+// }
+
+// implementation of ft_strdup
+// char	*ft_strdup(const char *src)
+// {
+// 	char	*dest;
+// 	size_t	i;
+
+// 	dest = (char *)malloc(ft_strlen(src) + 1);
+// 	if (!dest)
+// 		return (0);
+// 	i = 0;
+// 	while (src[i])
+// 	{
+// 		dest[i] = src[i];
+// 		i++;
+// 	}
+// 	dest[i] = '\0';
+// 	return (dest);
+// }
+
+// t_list	*ft_lstnew(void *content)
+// {
+// 	t_list	*head;
+
+// 	head = malloc(sizeof(t_list));
+// 	if (!head)
+// 		return (NULL);
+// 	head->content = content;
+// 	head->next = NULL;
+// 	return (head);
+// }
+
+// int main(void)
+// {
+// 	t_list	*head;
+// 	t_list	*new;
+
+// 	head = ft_lstnew(ft_strdup("Hello, "));
+// 	new = ft_lstnew(ft_strdup("World!"));
+// 	ft_lstadd_front(&head, new);  
+// 	while (head)
+// 	{
+// 		printf("%s", head->content);
+// 		head = head->next;
+// 	}
+// 	return (0);
+// }
