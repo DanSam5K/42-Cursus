2bf59fb2b2c81fe0bc76335ee8bd8af7b690fa77
Daniel Samuel <dansam5000@gmail.com>
2024-09-30 19:51:11 +0200

    comment out test code

--- Libft/srcs/bonus/ft_lstiter_bonus.c
+++ Libft/srcs/bonus/ft_lstiter_bonus.c
@@ -6,7 +6,7 @@
 /*   By: dansam <dansam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 20:17:55 by dsamuel           #+#    #+#             */
-/*   Updated: 2024/09/30 19:48:28 by dansam           ###   ########.fr       */
+/*   Updated: 2024/09/30 19:49:36 by dansam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,81 +26,81 @@ void	ft_lstiter(t_list *lst, void (*f)(void *))
 
 // for testing purposes only
 // // implementation of ft_strlen
-size_t	ft_strlen(const char *s)
-{
-	size_t	len;
+// size_t	ft_strlen(const char *s)
+// {
+// 	size_t	len;
 
-	len = 0;
-	while (s[len])
-		len++;
-	return (len);
-}
+// 	len = 0;
+// 	while (s[len])
+// 		len++;
+// 	return (len);
+// }
 
-// // implementation of ft_strdup
-char	*ft_strdup(const char *src)
-{
-	char	*dest;
-	size_t	i;
+// // // implementation of ft_strdup
+// char	*ft_strdup(const char *src)
+// {
+// 	char	*dest;
+// 	size_t	i;
 
-	dest = (char *)malloc(ft_strlen(src) + 1);
-	if (!dest)
-		return (0);
-	i = 0;
-	while (src[i])
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
+// 	dest = (char *)malloc(ft_strlen(src) + 1);
+// 	if (!dest)
+// 		return (0);
+// 	i = 0;
+// 	while (src[i])
+// 	{
+// 		dest[i] = src[i];
+// 		i++;
+// 	}
+// 	dest[i] = '\0';
+// 	return (dest);
+// }
 
-t_list	*ft_lstnew(void *content)
-{
-	t_list	*head;
+// t_list	*ft_lstnew(void *content)
+// {
+// 	t_list	*head;
 
-	head = malloc(sizeof(t_list));
-	if (!head)
-		return (NULL);
-	head->content = content;
-	head->next = NULL;
-	return (head);
-}
+// 	head = malloc(sizeof(t_list));
+// 	if (!head)
+// 		return (NULL);
+// 	head->content = content;
+// 	head->next = NULL;
+// 	return (head);
+// }
 
-void	ft_lstadd_back(t_list **lst, t_list *new)
-{
-	t_list	*temp;
+// void	ft_lstadd_back(t_list **lst, t_list *new)
+// {
+// 	t_list	*temp;
 
-	if (!lst || !new)
-		return ;
-	if (!(*lst))
-	{
-		*lst = new;
-		return ;
-	}
-	temp = *lst;
-	while (temp->next)
-		temp = temp->next;
-	temp->next = new;
-}
+// 	if (!lst || !new)
+// 		return ;
+// 	if (!(*lst))
+// 	{
+// 		*lst = new;
+// 		return ;
+// 	}
+// 	temp = *lst;
+// 	while (temp->next)
+// 		temp = temp->next;
+// 	temp->next = new;
+// }
 
-void	ft_putstr_fds(void *s)
-{
-	char	*str;
+// void	ft_putstr_fds(void *s)
+// {
+// 	char	*str;
 
-	str = (char *)s;
-	write(1, str, ft_strlen(str));
-	write(1, "\n", 1);
-}
+// 	str = (char *)s;
+// 	write(1, str, ft_strlen(str));
+// 	write(1, "\n", 1);
+// }
 
-int main(void)
-{
-	t_list	*head;
-	t_list	*new;
+// int main(void)
+// {
+// 	t_list	*head;
+// 	t_list	*new;
 
-	head = ft_lstnew(ft_strdup("Hello, "));
-	new = ft_lstnew(ft_strdup("World!"));
-	ft_lstadd_back(&head, new);
-	ft_lstiter(head, &ft_putstr_fds);
-	return (0);
-}
+// 	head = ft_lstnew(ft_strdup("Hello, "));
+// 	new = ft_lstnew(ft_strdup("World!"));
+// 	ft_lstadd_back(&head, new);
+// 	ft_lstiter(head, &ft_putstr_fds);
+// 	return (0);
+// }
