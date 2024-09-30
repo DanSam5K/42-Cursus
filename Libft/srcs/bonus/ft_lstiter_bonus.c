432b1727a731c59d058a531cc00a99436674ecb6
Daniel Samuel <dansam5000@gmail.com>
2024-09-30 19:49:27 +0200

    lstiter

--- Libft/srcs/bonus/ft_lstiter_bonus.c
+++ Libft/srcs/bonus/ft_lstiter_bonus.c
@@ -3,22 +3,104 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstiter_bonus.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dsamuel <dsamuel@student.42.fr>            +#+  +:+       +#+        */
+/*   By: dansam <dansam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 20:17:55 by dsamuel           #+#    #+#             */
-/*   Updated: 2024/06/18 20:20:37 by dsamuel          ###   ########.fr       */
+/*   Updated: 2024/09/30 19:48:28 by dansam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "../../includes/libft.h"
+
 
 void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
-	if (!f)
+	if (!lst || !f)
 		return ;
 	while (lst)
 	{
 		f(lst->content);
 		lst = lst->next;
 	}
+}
+
+// for testing purposes only
+// // implementation of ft_strlen
+size_t	ft_strlen(const char *s)
+{
+	size_t	len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
+}
+
+// // implementation of ft_strdup
+char	*ft_strdup(const char *src)
+{
+	char	*dest;
+	size_t	i;
+
+	dest = (char *)malloc(ft_strlen(src) + 1);
+	if (!dest)
+		return (0);
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*head;
+
+	head = malloc(sizeof(t_list));
+	if (!head)
+		return (NULL);
+	head->content = content;
+	head->next = NULL;
+	return (head);
+}
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*temp;
+
+	if (!lst || !new)
+		return ;
+	if (!(*lst))
+	{
+		*lst = new;
+		return ;
+	}
+	temp = *lst;
+	while (temp->next)
+		temp = temp->next;
+	temp->next = new;
+}
+
+void	ft_putstr_fds(void *s)
+{
+	char	*str;
+
+	str = (char *)s;
+	write(1, str, ft_strlen(str));
+	write(1, "\n", 1);
+}
+
+int main(void)
+{
+	t_list	*head;
+	t_list	*new;
+
+	head = ft_lstnew(ft_strdup("Hello, "));
+	new = ft_lstnew(ft_strdup("World!"));
+	ft_lstadd_back(&head, new);
+	ft_lstiter(head, &ft_putstr_fds);
+	return (0);
 }
