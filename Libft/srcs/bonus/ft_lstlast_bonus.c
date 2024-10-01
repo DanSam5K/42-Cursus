7b67524923a309db6ee94b6549f99b40fd95b7bc
Daniel Samuel <dansam5000@gmail.com>
2024-10-01 23:53:03 +0200

    lst last bonus

--- Libft/srcs/bonus/ft_lstlast_bonus.c
+++ Libft/srcs/bonus/ft_lstlast_bonus.c
@@ -3,18 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstlast_bonus.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dsamuel <dsamuel@student.42.fr>            +#+  +:+       +#+        */
+/*   By: dansam <dansam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 19:25:59 by dsamuel           #+#    #+#             */
-/*   Updated: 2024/06/18 19:29:12 by dsamuel          ###   ########.fr       */
+/*   Updated: 2024/10/01 23:52:11 by dansam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "../../includes/libft.h"
 
 t_list	*ft_lstlast(t_list *lst)
 {
 	while (lst && lst->next)
 		lst = lst->next;
 	return (lst);
+}
+
+int main()
+{
+	t_list *head;
+	t_list *node1;
+	t_list *node2;
+	t_list *node3;
 }
