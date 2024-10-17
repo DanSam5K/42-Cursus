0589cc96ef697d520c4aa5ecf28f2ba75efc51dd
Daniel Samuel <dansam5000@gmail.com>
2024-10-17 21:48:29 +0200

    test cases

--- Libft/srcs/bonus/ft_lstnew_bonus.c
+++ Libft/srcs/bonus/ft_lstnew_bonus.c
@@ -6,7 +6,7 @@
 /*   By: dansam <dansam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 18:49:25 by dsamuel           #+#    #+#             */
-/*   Updated: 2024/10/17 21:43:52 by dansam           ###   ########.fr       */
+/*   Updated: 2024/10/17 21:47:39 by dansam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,106 +26,12 @@ t_list	*ft_lstnew(void *content)
 
 // for testing purposes only
 // implementation of ft_strlen
-size_t	ft_strlen(const char *s)
-{
-	size_t	len;
-
-	len = 0;
-	while (s[len])
-		len++;
-	return (len);
-}
-
-// implementation of ft_strdup
-char	*ft_strdup(const char *src)
-{
-	char	*dest;
-	size_t	i;
-
-	dest = (char *)malloc(ft_strlen(src) + 1);
-	if (!dest)
-		return (0);
-	i = 0;
-	while (src[i])
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-
-// t_list	*ft_lstnew(void *content)
-t_list	*ft_lstnew(void *content)
-{
-	t_list	*head;
-
-	head = malloc(sizeof(t_list));
-	if (!head)
-		return (NULL);
-	head->content = content;
-	head->next = NULL;
-	return (head);
-}
-
-// void	ft_lstadd_front(t_list **lst, t_list *new)
-void	ft_lstadd_front(t_list **lst, t_list *new)
-{
-	if (!lst || !new)
-		return ;
-	new->next = *lst;
-	*lst = new;
-}
-
-// void	ft_lstadd_back(t_list **lst, t_list *new)
-void	ft_lstadd_back(t_list **lst, t_list *new)
-{
-	t_list	*temp;
-
-	if (!lst || !new)
-		return ;
-	if (!(*lst))
-	{
-		*lst = new;
-		return ;
-	}
-	temp = *lst;
-	while (temp->next)
-		temp = temp->next;
-	temp->next = new;
-}
-
-// t_list	*ft_lstlast(t_list *lst)
-t_list	*ft_lstlast(t_list *lst)
-{
-	if (!lst)
-		return (NULL);
-	while (lst->next)
-		lst = lst->next;
-	return (lst);
-}
-
-// void	ft_lstdelone(t_list *lst, void (*del)(void *))
-void	ft_lstdelone(t_list *lst, void (*del)(void *))
-{
-	if (!lst)
-		return ;
-	if (del)
-		del(lst->content);
-	free(lst);
-}
-
-// void	ft_lstclear(t_list **lst, void (*del)(void *))
-void	ft_lstclear(t_list **lst, void (*del)(void *))
-{
-	t_list	*temp;
-
-	if (!lst || !del)
-		return ;
-	while (*lst)
-	{
-		temp = (*lst)->next;
-		ft_lstdelone(*lst, del);
-		*lst = temp;
-	}
-}
+// int main(void)
+// {
+// 	t_list *head = ft_lstnew(ft_strdup("Hello"));
+// 	ft_lstadd_back(&head, ft_lstnew(ft_strdup("World")));
+// 	ft_lstadd_back(&head, ft_lstnew(ft_strdup("42")));
+// 	printf("head->content: %s\n", (char *)head->content);
+// 	ft_lstclear(&head, free);
+// 	return (0);
+// }
