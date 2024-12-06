d10a7aa1ebbbeba6c2c301caccbc75ac931b2779
Daniel Samuel <dsamuel@student.42wolfsburg.de>
2024-12-06 18:49:01 +0100

    Header file

---
+++ cpp_modules/CPP00/ex01/header.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   header.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dsamuel <dsamuel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/06 18:41:08 by dsamuel           #+#    #+#             */
+/*   Updated: 2024/12/06 18:47:56 by dsamuel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <iomanip>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sstream>
+#include "PhoneBook.hpp"
+#include "Contact.hpp"
+
+# define MaxLength 10
+# define MaxId 8
