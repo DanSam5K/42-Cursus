20bb2fae785fc8026dc45a9c84a2a65dc89c6095
Daniel Samuel <dsamuel@student.42wolfsburg.de>
2024-12-06 17:03:33 +0100

    rank 4 microshell

--- Exams/rank-4/microshell.c
+++ Exams/rank-4/microshell.c
@@ -6,7 +6,7 @@
 /*   By: dsamuel <dsamuel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 17:47:48 by dsamuel           #+#    #+#             */
-/*   Updated: 2024/11/26 13:03:06 by dsamuel          ###   ########.fr       */
+/*   Updated: 2024/12/06 17:02:57 by dsamuel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,15 @@ void	err(char *str)
 {
 	while (*str)
 		write(2, str++, 1);
+}
+
+int	cd(char **argv, int i)
+{
+	if (i != 2)
+		return (err("error: cd: bad arguments\n"), 1);
+	if (chdir(argv[1]) == -1)
+		return (err("error: cd: cannot change directory to "),
+			err(argv[1]), err("\n"), 1);
+	return (0);
 }
 
