5657f8fdf6b15c2bf45e7fc7378a4b04e71713c4
Daniel Samuel <dsamuel@student.42wolfsburg.de>
2024-12-10 11:18:04 +0100

    main program entry point

---
+++ minishell/src/main/minishell.c
@@ -0,0 +1,129 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ojacobs <ojacobs@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/09 20:56:48 by dsamuel           #+#    #+#             */
+/*   Updated: 2024/11/27 22:03:47 by ojacobs          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+t_sig_handler	g_global_sig;
+
+void	ft_redir_and_exec(t_shell_state *shell_state, t_cmd_token *cmd_token)
+{
+	t_cmd_token	*prev;
+	t_cmd_token	*next;
+	int			pipe;
+
+	prev = ft_prev_sep(cmd_token, NOSKIP);
+	next = ft_next_sep(cmd_token, NOSKIP);
+	pipe = 0;
+	if (ft_is_type(prev, HERE_DOC) && shell_state->here_doc_triggered == 0)
+	{
+		shell_state->here_doc_triggered = 1;
+		ft_here_doc(shell_state, cmd_token);
+	}
+	else if (ft_is_type(prev, TRUNC))
+		ft_redir(shell_state, cmd_token, TRUNC);
+	else if (ft_is_type(prev, APPEND))
+		ft_redir(shell_state, cmd_token, APPEND);
+	else if (ft_is_type(prev, INPUT))
+		ft_input(shell_state, cmd_token);
+	else if (ft_is_type(prev, PIPE))
+		pipe = ft_mini_pipe(shell_state);
+	if (next && ft_is_type(next, END) == 0 && pipe != 1)
+		ft_redir_and_exec(shell_state, next->next);
+	if ((ft_is_type(prev, END) || ft_is_type(prev, PIPE) || \
+	!prev) && pipe != 1 && shell_state->should_skip_exec == 0)
+		ft_exec_cmd(shell_state, cmd_token);
+}
+
+static void	ft_set_shell(t_shell_state *shell_state, t_cmd_token *cmd_token)
+{
+	shell_state->is_foreground = 1;
+	shell_state->is_parent_proc = 1;
+	shell_state->last_exit_stat = 1;
+	ft_redir_and_exec(shell_state, cmd_token);
+	ft_reset_std(shell_state);
+	ft_close_fds(shell_state);
+	ft_reset_fds(shell_state);
+}
+
+void	ft_mini_shell(t_shell_state *shell_state)
+{
+	t_cmd_token	*cmd_token;
+	int			status;
+
+	cmd_token = ft_next_run(shell_state->cmd_list, NOSKIP);
+	if (ft_is_types(shell_state->cmd_list, "TAI"))
+		cmd_token = shell_state->cmd_list->next;
+	else
+		cmd_token = cmd_token;
+	while (shell_state->should_exit == 0 && cmd_token)
+	{
+		ft_set_shell(shell_state, cmd_token);
+		waitpid(-1, &status, 0);
+		status = WEXITSTATUS(status);
+		if (shell_state->last_exit_stat == 0)
+			shell_state->return_code = status;
+		else
+			shell_state->return_code = shell_state->return_code;
+		if (shell_state->is_parent_proc == 0)
+		{
+			ft_free_token(shell_state->cmd_list);
+			exit(shell_state->return_code);
+		}
+		shell_state->should_skip_exec = 0;
+		cmd_token = ft_next_run(cmd_token, SKIP);
+	}
+}
+
+static void	ft_init_shell(t_shell_state	*shell_state)
+{
+	shell_state->stdin_fd = dup(STDIN);
+	shell_state->stdout_fd = dup(STDOUT);
+	shell_state->active_env = NULL;
+	shell_state->secret_env = NULL;
+	shell_state->cmd_list = NULL;
+	shell_state->tilde = NULL;
+	shell_state->is_foreground = 0;
+	shell_state->should_exit = 0;
+	shell_state->return_code = 0;
+	shell_state->should_skip_exec = 0;
+	shell_state->history_count = 0;
+	shell_state->here_doc_triggered = 0;
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	t_shell_state	shell_state;
+
+	(void)argc;
+	(void)argv;
+	ft_init_shell(&shell_state);
+	ft_reset_fds(&shell_state);
+	ft_env_init(&shell_state, envp);
+	ft_secret_env_init(&shell_state, envp);
+	ft_increment_shell_level(shell_state.active_env);
+	while (shell_state.should_exit == 0)
+	{
+		ft_sig_init();
+		if (ft_parse_input(&shell_state) == 1)
+			continue ;
+		if (shell_state.cmd_list != NULL
+			&& ft_check_line(&shell_state, shell_state.cmd_list))
+			ft_mini_shell(&shell_state);
+		ft_free_token(shell_state.cmd_list);
+	}
+	ft_free_history(shell_state);
+	if (shell_state.active_env)
+		ft_free_env(shell_state.active_env);
+	ft_free_env(shell_state.secret_env);
+	ft_memdel(shell_state.tilde);
+	return (shell_state.return_code);
+}
---
+++ minishell/src/main/redirections.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   redirections.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ojacobs <ojacobs@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/24 15:28:09 by ojacobs           #+#    #+#             */
+/*   Updated: 2024/11/17 18:23:12 by ojacobs          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	ft_redir(t_shell_state *shell_state, t_cmd_token *token, int type)
+{
+	ft_close(shell_state->output_fd);
+	if (type == TRUNC)
+		shell_state->output_fd = open(token->content, O_CREAT
+				| O_WRONLY | O_TRUNC, S_IRWXU);
+	else
+		shell_state->output_fd = open(token->content, O_CREAT
+				| O_WRONLY | O_APPEND, S_IRWXU);
+	if (shell_state->output_fd == -1)
+	{
+		ft_putstr_fd("minishell: ", STDERR);
+		ft_putstr_fd(token->content, STDERR);
+		ft_putendl_fd(": No such file or directory", STDERR);
+		shell_state->return_code = 1;
+		shell_state->should_skip_exec = 1;
+		return ;
+	}
+	dup2(shell_state->output_fd, STDOUT);
+}
+
+void	ft_input(t_shell_state *shell_state, t_cmd_token *token)
+{
+	if (ft_strcmp(token->content, "<") == 0)
+	{
+		if (token->next == NULL || token->next->type >= TRUNC)
+		{
+			ft_putstr_fd("syntax error near unexpected token `newline'\n", \
+			STDERR);
+			shell_state->return_code = 258;
+			shell_state->should_skip_exec = 1;
+			return ;
+		}
+		token = token->next;
+	}
+	ft_close(shell_state->input_fd);
+	shell_state->input_fd = open(token->content, O_RDONLY, S_IRWXU);
+	if (shell_state->input_fd == -1)
+	{
+		ft_putstr_fd("minishell: ", STDERR);
+		ft_putstr_fd(token->content, STDERR);
+		ft_putendl_fd(": No such file or directory", STDERR);
+		shell_state->return_code = 1;
+		shell_state->should_skip_exec = 1;
+		return ;
+	}
+	dup2(shell_state->input_fd, STDIN);
+}
+
+int	ft_mini_pipe(t_shell_state *shell_state)
+{
+	pid_t	pid;
+	int		pipe_fd[2];
+
+	pipe(pipe_fd);
+	pid = fork();
+	if (pid == 0)
+	{
+		ft_close(pipe_fd[1]);
+		dup2(pipe_fd[0], STDIN);
+		shell_state->pipe_input_fd = pipe_fd[0];
+		shell_state->proc_id = -1;
+		shell_state->is_parent_proc = 0;
+		shell_state->should_skip_exec = 0;
+		return (2);
+	}
+	else
+	{
+		ft_close(pipe_fd[0]);
+		dup2(pipe_fd[1], STDOUT);
+		shell_state->pipe_output_fd = pipe_fd[1];
+		shell_state->proc_id = pid;
+		shell_state->last_exit_stat = 0;
+		return (1);
+	}
+}
