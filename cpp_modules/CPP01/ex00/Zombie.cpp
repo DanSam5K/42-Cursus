dc5574146dfe1ddfdefd92a216a28747460c1434
Daniel Samuel <dsamuel@student.42wolfsburg.de>
2024-12-13 18:34:19 +0100

    zombie cpp

---
+++ cpp_modules/CPP01/ex00/Zombie.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dsamuel <dsamuel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/13 18:09:53 by dsamuel           #+#    #+#             */
+/*   Updated: 2024/12/13 18:15:00 by dsamuel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+void Zombie::announce(void) {
+    std::cout << "<" << this->name << "> BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
+Zombie::Zombie(const std::string &inputname) {
+    this->name = inputname;
+}
+
+Zombie::~Zombie() {
+    std::cout << "<" << this->name << "> is dead" << std::endl;
+}
