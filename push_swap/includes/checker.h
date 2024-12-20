b9e2c84ea8295434dd867ee0df1c51f4d40dd7f2
Daniel Samuel <dansam5000@gmail.com>
2024-12-20 16:58:24 +0100

    checker header

---
+++ push_swap/includes/checker.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checker.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dsamuel <dsamuel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/16 13:42:03 by dsamuel           #+#    #+#             */
+/*   Updated: 2024/09/02 12:19:09 by dsamuel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CHECKER_H
+# define CHECKER_H
+
+# include "ft_printf.h"
+# include "libft.h"
+# include "push_swap.h"
+# include <stdbool.h>
+
+int					stack_size(t_node *stack);
+
+#endif
