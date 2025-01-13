1258683f70783241c53a6e96653249c4a280587e
Daniel Samuel <dsamuel@student.42wolfsburg.de>
2025-01-13 15:18:05 +0100

    add src files

---
+++ cpp_modules/CPP04/ex02/src/Animal.cpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dsamuel <dsamuel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/09 20:12:48 by dsamuel           #+#    #+#             */
+/*   Updated: 2025/01/09 20:13:06 by dsamuel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Animal.hpp"
+
+Animal::Animal() {
+  std::cout << "Animal -> Default constructor called!\n";
+  this->type = "Animauuu";
+}
+
+Animal::Animal(const Animal &other) {
+  std::cout << "Animal -> Copy constructor called!\n";
+  this->type = other.type;
+}
+
+Animal &Animal::operator=(const Animal &other) {
+  std::cout << "Animal -> Copy assigment operator called!\n";
+  if (this == &other)
+    return *this;
+  this->type = other.type;
+  return *this;
+}
+
+Animal::~Animal() { std::cout << "Animal -> Destructor called!\n"; }
+
+void Animal::makeSound() const {
+  std::cout
+      << "I am a general animal and I don´t make sounds. Implement me somewhere"
+      << std::endl;
+}
+
+std::string Animal::getType() const { return type; }
