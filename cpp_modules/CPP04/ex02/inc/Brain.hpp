2c3e8394b80f05e78946c961a67e34bc9ca9b397
Daniel Samuel <dsamuel@student.42wolfsburg.de>
2025-01-13 15:17:41 +0100

    includes function ex03

---
+++ cpp_modules/CPP04/ex02/inc/Brain.hpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Brain.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dsamuel <dsamuel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/09 20:10:05 by dsamuel           #+#    #+#             */
+/*   Updated: 2025/01/09 20:10:10 by dsamuel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BRAIN_HPP
+#define BRAIN_HPP
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+#define IDEAS 100
+
+class Brain {
+private:
+  std::string ideas[IDEAS];
+
+public:
+  /**
+   * @brief Default constructor.
+   */
+  Brain();
+  /**
+   * @brief Copy constructor.
+   * @param other The other Brain to copy.
+   */
+  Brain(const Brain &other);
+  /**
+   * @brief Copy assignment operator.
+   * @param other The other Brain to assign.
+   * @return A reference to the assigned Brain.
+   */
+  Brain &operator=(const Brain &other);
+  /**
+   * @brief Destructor.
+   */
+  ~Brain();
+  /**
+   * @brief Fill the brain with 25 random ideas.
+   */
+  void RandomIdeas();
+};
+
+#endif // BRAIN_HPP
---
+++ cpp_modules/CPP04/ex02/inc/WrongCat.hpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   WrongCat.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dsamuel <dsamuel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/09 20:11:29 by dsamuel           #+#    #+#             */
+/*   Updated: 2025/01/09 20:11:32 by dsamuel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WRONGCAT_HPP
+#define WRONGCAT_HPP
+
+#include "WrongAnimal.hpp"
+
+class WrongCat : public WrongAnimal {
+public:
+  /**
+   * @brief Default constructor.
+   */
+  WrongCat();
+  /**
+   * @brief Copy constructor.
+   * @param other The other WrongCat to copy.
+   */
+  WrongCat(const WrongCat &other);
+  /**
+   * @brief Copy assignment operator.
+   * @param other The other WrongCat to assign.
+   * @return A reference to the assigned WrongCat.
+   */
+  WrongCat &operator=(const WrongCat &other);
+  /**
+   * @brief Destructor.
+   */
+  ~WrongCat();
+  /**
+   * @brief Prints a custom message with a Sound from this class.
+   */
+  void makeSound();
+};
+
+#endif // WRONGCAT_HPP
