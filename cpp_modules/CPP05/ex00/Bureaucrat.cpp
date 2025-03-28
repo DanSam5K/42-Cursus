0584e7e322f995311c14f29e4e672c4aebde58a5
Daniel Samuel <dsamuel@student.42wolfsburg.de>
2025-03-28 14:33:47 +0100

    implment bureaucrat functions

---
+++ cpp_modules/CPP05/ex00/Bureaucrat.cpp
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dsamuel <dsamuel@student.42wolfsburg.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/28 13:22:22 by dsamuel           #+#    #+#             */
+/*   Updated: 2025/03/28 14:06:45 by dsamuel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+// Constructor with grade validation
+Bureaucrat::Bureaucrat(const std::string name, int grade) : name(name)
+{
+    if (grade < 1)
+        throw Bureaucrat::GradeTooHighException();
+    if (grade > 150)
+        throw Bureaucrat::GradeTooLowException();
+    this->grade = grade;
+}
+
+// Destructor
+Bureaucrat::~Bureaucrat() {}
+
+// Getters
+std::string Bureaucrat::getName() const { return name; }
+
+int Bureaucrat::getGrade() const { return grade; }
+
+// Functions  Grade modifications  
+void Bureaucrat::incrementGrade()
+{
+    if (grade - 1 < 1)
+        throw GradeTooHighException();
+    grade--;
+}
+
+void Bureaucrat::decrementGrade()
+{
+    if (grade + 1 > 150)
+        throw GradeTooLowException();
+    grade++;
+}
+
+// Exception msg
+const char *Bureaucrat::GradeTooHighException::what() const throw()
+{
+    return "Grade is too high!";
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const throw()
+{
+    return "Grade is too low!";
+}
+
+// Overload << operator for printing
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &bureaucrat)
+{
+    out << bureaucrat.getName() << ", bureaucrat grade " << bureaucrat.getGrade() << std::endl;
+    return out;
+}
