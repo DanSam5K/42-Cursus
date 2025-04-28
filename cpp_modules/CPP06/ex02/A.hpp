6bb34d6652ca96f97fee06f9562169267adc7444
Daniel Samuel <dsamuel@student.42wolfsburg.de>
2025-04-28 10:36:41 +0200

    Build << A, B & C clasess prototype

---
+++ cpp_modules/CPP06/ex02/A.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   A.hpp                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dsamuel <dsamuel@student.42wolfsburg.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/23 16:18:06 by dsamuel           #+#    #+#             */
+/*   Updated: 2025/04/23 16:28:15 by dsamuel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EX02_A_HPP
+#define EX02_A_HPP
+
+#include "Base.hpp"
+
+// Class A inherits from Base and represents one of the derived types.
+// It does not introduce any additional members or behavior.
+class A : public Base 
+{
+    // No additional members or functions
+};
+
+#endif
